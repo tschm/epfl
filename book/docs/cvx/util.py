5a05d7d009f59e3936eb16da83a6c604948f1a58
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-05-25 17:54:47 +0400

    typehints

--- book/docs/cvx/util.py
+++ book/docs/cvx/util.py
@@ -1,15 +1,22 @@
 import numpy as np
 import cvxpy as cvx
+from typing import List, Optional
 
 
-def minimize(objective, constraints=None):
+def minimize(
+    objective: cvx.Expression,
+    constraints: Optional[List[cvx.constraints.constraint.Constraint]] = None,
+) -> float:
     return cvx.Problem(cvx.Minimize(objective), constraints).solve()
 
 
-def maximize(objective, constraints=None):
+def maximize(
+    objective: cvx.Expression,
+    constraints: Optional[List[cvx.constraints.constraint.Constraint]] = None,
+) -> float:
     return cvx.Problem(cvx.Maximize(objective), constraints).solve()
 
 
-def std(vector):
+def std(vector: cvx.Expression) -> cvx.Expression:
     # we assume here that the vector is centered
     return cvx.norm(vector, 2) / np.sqrt(vector.size[0] - 1)
