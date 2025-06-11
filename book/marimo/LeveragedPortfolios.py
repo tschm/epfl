e19acfd4cc21f73bcb32afe0bbd023f3b403c873
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-06-11 16:24:10 +0400

    dependencies local

--- book/marimo/LeveragedPortfolios.py
+++ book/marimo/LeveragedPortfolios.py
@@ -1,3 +1,13 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "cvxpy-base==1.6.6",
+#     "marimo==0.13.15",
+#     "numpy==2.3.0",
+#     "clarabel==0.11.0",
+# ]
+# ///
+
 import marimo
 
 __generated_with = "0.13.15"
